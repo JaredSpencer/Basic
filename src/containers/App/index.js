c874df834dc52282436b783ad1863343a3a0c730
Jared Spencer <jaredspencer@Jareds-MacBook-Pro.local>
2018-07-24 21:40:03 -0700

    Finished app cleanup and setup with Semantic UI

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
 import FormContainer from '../Form';
+import {Container, Button, Icon} from 'semantic-ui-react';
 
 export default function App() {
-	return (
-		<div>
-			"Hello There"
-		</div>
-	);
+  return (
+    <Container>
+      <Button size='small' color='green'>
+        <Icon name='download'/>
+        I'm a Button!
+      </Button>
+    </Container>
+  );
 }
-
-// return (
-// 	<div>
-// 		<FormContainer />
-// 	</div>
-// );
